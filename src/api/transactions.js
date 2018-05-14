cd9420996f06bee47d25dad4d8930a965119e852
Yongfeng LI <wliyongfeng@gmail.com>
2018-05-14 18:42:06 +0800

    add transaction api

---
+++ src/api/transactions.js
@@ -0,0 +1,23 @@
+const transactions = connection => {
+  return {
+    build: (baseTransaction = null, actions, ttl = 0) => connection.request('/build-transaction', {
+      base_transaction: baseTransaction,
+      actions,
+      ttl
+    }),
+
+    sign: (transaction, password) => connection.request('/sign-transaction', {transaction, password}),
+
+    submit: (rawTransaction) => connection.request('/submit-transaction', {raw_transaction: rawTransaction}),
+
+    estimateGas: (transaction) => connection.request('/estimate-transaction-gas', {
+      transaction_template: transaction
+    }),
+
+    listAll: () => connection.request('/list-transactions', {}),
+
+    listById: (id) => connection.request('/list-transactions', {id}),
+
+    listByAccountId: (accountId) => connection.request('list-transactions', {account_id: accountId})
+  }
+}
