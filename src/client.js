00e1368365dfd091f99de25eed40e08a92f817b6
Yongfeng LI <wliyongfeng@gmail.com>
2018-05-14 18:49:21 +0800

    add asset/key/transaction api to client

--- src/client.js
+++ src/client.js
@@ -1,11 +1,17 @@
 import Connection from 'connection'
 import AccountsApi from 'api/account'
+import AssetApi from 'api/assets'
+import KeysApi from 'api/keys'
+import TransactionApi from 'api/transactions'
 
 class Client {
   constructor(baseUrl, token) {
     this.connection = new Connection(baseUrl, token)
 
-    this.account = new AccountsApi(this.connection)
+    this.accounts = new AccountsApi(this.connection)
+    this.assets = new AssetApi(this.connection)
+    this.keys = new KeysApi(this.connection)
+    this.transactions = new TransactionApi(this.connection)
   }
 }
 
